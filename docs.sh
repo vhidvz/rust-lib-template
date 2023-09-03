14cf7b1be96d9ab307be3b27e773e37feb5c6963
Vahid V. <vhid.vz@gmail.com>
2023-09-03 23:02:32 +0330

    update

--- docs.sh
+++ docs.sh
@@ -1,8 +1,8 @@
 # Test
-CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='target/cargo-test.profraw' cargo test
+CARGO_INCREMENTAL=0 RUSTFLAGS="-Cinstrument-coverage" LLVM_PROFILE_FILE="target/cargo-test.profraw" cargo test
 
 # Coverage
-grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing -o docs/coverage
+grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore "../*" -o docs/coverage
 
 # Documentation
 cargo doc && cp -r ./target/doc/. ./docs
--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,18 @@ pub fn add(left: usize, right: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
+    extern crate dotenv;
+
     use super::*;
+
+    fn init() {
+        dotenv::dotenv().ok();
+    }
 
     #[test]
     fn it_works() {
+        tests::init();
+
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
